0c4b3b4f6baf09d3ed7cc1428d803e855ae982bd
Bob Obringer <bobringer@grubhub.com>
2020-06-20 19:42:35 -0400

    fixed react import

--- react.js
+++ react.js
@@ -20,11 +20,12 @@ module.exports = {
         "react/boolean-prop-naming": ["error", { "rule": "^(is|has|should)[A-Z]([A-Za-z0-9]?)+" }],
         "react/jsx-filename-extension": ["error", { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
         "react/jsx-handler-names": "error",
-        "react/jsx-indent": ["error", "tab"],
-        "react/jsx-indent-props": ["error", "tab"],
+        "indent": ["error", 4, {"ignoredNodes": ["JSXElement"]}],
+        "react/jsx-indent": ["off"],
+        "react/jsx-indent-props": ["off"],
         "react/jsx-key": "error",
         "react/jsx-max-depth": ["error", {
-            "max": 3
+            "max": 2
         }],
         "react/jsx-no-literals": "error",
         "react/no-unsafe": "error",
